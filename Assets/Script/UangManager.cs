cbadbe9370cf0f3121708f56c14bf68b3cf639c6
AndriesD <andriesjavier4@gmail.com>
2026-08-17 19:50:41 +0800

    First Commit

---
+++ Assets/Script/UangManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using TMPro;
+using Yarn.Unity; // Wajib dipanggil agar bisa berkomunikasi dengan Yarn
+
+public class UangManager : MonoBehaviour
+{
+    public TextMeshProUGUI teksUang;
+    private int saldo = 5000000; // Uang awal Raka
+
+    void Start()
+    {
+        UpdateLayar();
+    }
+
+    // [YarnCommand] membuat fungsi ini bisa dipanggil langsung dari teks naskah Yarn!
+    [YarnCommand("potong_uang")]
+    public void PotongUang(int jumlahPotongan)
+    {
+        saldo = saldo - jumlahPotongan;
+        UpdateLayar();
+    }
+
+    void UpdateLayar()
+    {
+        // Menampilkan angka dengan format ribuan (titik)
+        teksUang.text = "Portofolio: Rp " + saldo.ToString("N0");
+    }
+}
